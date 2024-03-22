95854bb782e83177f8ffcd91547de6739028a3e7
FahaTrader <159493266+FahaTrader@users.noreply.github.com>
2024-03-21 21:14:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ checkoutBtn.addEventListener("click", function(){
 function checkRestaurantOpen(){
     const data = new Date();
     const hora = data.getHours();
-    return hora >= 18 && hora < 23;
+    return hora >= 18 && hora < 20;
 }
 
 const spanItem = document.getElementById("date-span")
